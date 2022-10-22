a32675dfffba91d133e379a4fe55efc4acb02b10
alanderekh <alanderekh@gmail.com>
2022-10-22 08:45:43 -0600

    Agregado extra

---
+++ app/src/main/java/com/alanhenriquez/bubblepet2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.alanhenriquez.bubblepet2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xforce.bubblepet2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
